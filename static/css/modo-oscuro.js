aa0647c1542768f7dfd3f49334c6b5983a1b73a4
emirgonzalez55 <emigonzalez55@gmail.com>
2022-08-15 06:55:02 -0300

    Funciones mejoradas

--- static/css/modo-oscuro.js
+++ static/css/modo-oscuro.js
@@ -3,27 +3,37 @@ const modooscuro = document.getElementById('modooscuro');
 const boton = document.querySelector('#boton');
 const localConfig = localStorage.getItem('tema');
 
-
 if (localConfig === 'dark') {
     modooscuro.classList.add('modo-oscuro')
-    tematable.classList.add('table-dark')
 
 } else if (localConfig === 'light') {
     modooscuro.classList.remove('modo-oscuro')
+}
+if (localConfig === 'dark' && tematable !=null) {
+    tematable.classList.add('table-dark')
+    
+}else if (localConfig === 'light' && tematable != null) {
     tematable.classList.remove('table-dark')
 }
 boton.addEventListener('click', () => {
     let colorTema;
-    if (modooscuro.classList.contains('modo-oscuro')){
+    if (tematable === null && modooscuro.classList.contains('modo-oscuro')){
+        modooscuro.classList.remove('modo-oscuro')
+        colorTema = 'light'
+    }
+    else if(tematable != null && modooscuro.classList.contains('modo-oscuro')){
         modooscuro.classList.remove('modo-oscuro')
         tematable.classList.remove('table-dark')
         colorTema = 'light'
-
-    }else {
-        modooscuro.classList.add('modo-oscuro',)
+    }
+    else if(tematable === null  && !modooscuro.classList.contains('modo-oscuro')){
+        modooscuro.classList.add('modo-oscuro')
+        colorTema = 'dark'
+    }
+    else if(tematable != null  && !modooscuro.classList.contains('modo-oscuro')){
+        modooscuro.classList.add('modo-oscuro')
         tematable.classList.add('table-dark')
         colorTema = 'dark'
     }
     localStorage.setItem('tema', colorTema)
 }) 
-    
